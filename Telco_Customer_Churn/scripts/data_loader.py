908a0eb093a096563ccefa7a8f3411f144122fba
navyaraghunath31 <navyaraghunath31@gmail.com>
2025-08-14 20:05:10 +0530

    Update data_loader.py

--- Telco_Customer_Churn/scripts/data_loader.py
+++ Telco_Customer_Churn/scripts/data_loader.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 def load_data():
-    df = pd.read_csv('data/Telco-Customer-Churn.csv')
+    df = pd.read_csv('Telco-Customer-Churn/data/Telco-Customer-Churn.csv')
     if 'customerID' in df.columns:
         df = df.drop('customerID', axis=1)
     df = df.replace(' ', np.nan)
     df = df.dropna()
     df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
     return df
+
